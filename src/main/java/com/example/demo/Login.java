6f4e51e6621260183e51bf321fc698d2ba8caca2
Tran Quoc Thinh <tranquocthinh2221980@gmail.com>
2026-02-13 15:01:37 +0700

    updated

--- src/main/java/com/example/demo/Login.java
+++ src/main/java/com/example/demo/Login.java
@@ -1,28 +1,17 @@
+package com.example.demo;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 class LoginController {
 
-    @PostMapping("/login")
-    public String login(@RequestParam String username,
-                        @RequestParam String password) {
-
-        // SAST test
-        if (username.equals("admin") && password.equals("admin123")) {
-            return "Login success";
-        }
-
-        return "Login failed";
-    }
-
     @GetMapping("/hello")
-    public String hello(@RequestParam String name) {
-
-        // XSS test (DAST test)
-        return "<h1>Hello " + name + "</h1>";
+    public ResponseEntity<String> hello(@RequestParam String name) {
+        return ResponseEntity.ok().contentType(MediaType.TEXT_HTML)
+            .body("<html><body>Hello " + name + "</body></html>");
     }
+
 }
